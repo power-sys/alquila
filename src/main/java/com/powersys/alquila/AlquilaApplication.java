48cca70272d9905ef5c762e988e79eaeb8176730
Nicolas Villalba <nerynicolas@gmail.com>
2018-11-03 18:18:27 -0300

    initial commit, starting alquila project

---
+++ src/main/java/com/powersys/alquila/AlquilaApplication.java
@@ -0,0 +1,12 @@
+package com.powersys.alquila;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlquilaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlquilaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/powersys/alquila/AlquilaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.powersys.alquila;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AlquilaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
