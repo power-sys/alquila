70da0a621da38a40fe995628ecf94b0dd6f5d032
AmadeoO <amadeo.fgarcia@gmail.com>
2018-11-13 09:30:07 -0300

    annotations were added for the scan of packages

--- src/main/java/com/powersys/alquila/AlquilaApplication.java
+++ src/main/java/com/powersys/alquila/AlquilaApplication.java
@@ -2,8 +2,14 @@ package com.powersys.alquila;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages= {"com.powersys.alquila"})
+@EnableJpaRepositories("com.powersys.alquila.repositories")
+@EntityScan(basePackages= {"com.powersys.alquila.domain"})
 public class AlquilaApplication {
 
 	public static void main(String[] args) {
